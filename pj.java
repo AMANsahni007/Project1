e5fefbdc0a4be22c7ed6937c243cec342f524073
AMAN SAHNI <153723094+AMANsahni007@users.noreply.github.com>
2025-01-28 11:16:38 +0530

    new

--- pj.java
+++ pj.java
@@ -1,11 +1,15 @@
 import java.util.Scanner;
-
 public class pj {
     public static void main(String[] args) {
+        int arr[9];
+        int m,i;
         Scanner sc= new Scanner(System.in);
         int n=sc.nextInt();
-        for(int i=0;i<n;i++)
-        System.out.println("Aman");
+        for(i=0;i<n;i++)
+        m=sc.nextInt();
+        arr[i]=m;
+        System.out.println();
+
     }
     
 }
